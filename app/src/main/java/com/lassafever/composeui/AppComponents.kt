73bafee69ddbcea90972cab7bf1cd0aeca516d9e
Israela608 <israela608@gmail.com>
2025-07-12 19:32:46 +0100

    Button

--- app/src/main/java/com/lassafever/composeui/AppComponents.kt
+++ app/src/main/java/com/lassafever/composeui/AppComponents.kt
@@ -7,14 +7,18 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.TextUnit
@@ -88,6 +92,7 @@ fun SimpleButton(){
         onClick = {
             Log.d("SimpleButton", "Button Clicked!")
         },
+        shape = RoundedCornerShape(12.dp)
     ){
         NormalTextForButtons("Click here")
     }
@@ -106,6 +111,7 @@ fun NormalTextForButtons(value: String){
         modifier = Modifier
             .fillMaxWidth()
             .padding(8.dp),
-        text = value
+        text = value,
+        textAlign = TextAlign.Center,
     )
 }
